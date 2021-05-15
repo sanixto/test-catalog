c2acbfbe74762eb5125b9bde686c449121b3d2e0
sanixto <sanix.to@gmail.com>
2021-05-15 19:06:17 +0300

    update localStorageUtil

--- utils/localStorageUtil.js
+++ utils/localStorageUtil.js
@@ -1,39 +1,33 @@
 class LocalStorageUtil {
-    constructor() {
-        this.keyName = 'products';
+  constructor() {
+      this.keyName = 'products';
+  }
+
+  getProducts() {
+    const productsLocalStorage = localStorage.getItem(this.keyName);
+ 
+    if (productsLocalStorage !== null) {
+      return JSON.parse(productsLocalStorage);
+    }
+    return [];
+  }
+
+  putProducts(id) {
+    let products = this.getProducts();
+    let pushProduct = false;
+    const index = products.indexOf(id);
+
+    if (index === -1) {
+      products.push(id);
+      pushProduct = true;
+    } else {
+      products.splice(index, 1);
     }
 
-    getProducts() {
-        const productsLocalStorage = localStorage.getItem(this.keyName);
-        if (productsLocalStorage !== null) {
-            return JSON.parse(productsLocalStorage);
-        }
-        return [];
-    }
+    localStorage.setItem(this.keyName, JSON.stringify(products));
 
-    putProducts(id) {
-        let products = this.getProducts();
-        let pushProduct = false;
-        const index = products.indexOf(id);
-
-        if (index === -1) {
-            products.push(id);
-            pushProduct = true;
-        } else {
-            products.splice(index, 1);
-        }
-
-        localStorage.setItem(this.keyName, JSON.stringify(products));
-
-        return { pushProduct, products }
+    return { pushProduct, products }
     }
 }
 
 const localStorageUtil = new LocalStorageUtil();
-
-
-localStorageUtil.putProducts(`el1`);
-localStorageUtil.putProducts(`el2`);
-localStorageUtil.putProducts(`el3`);
-localStorageUtil.putProducts(`el4`);
-localStorageUtil.putProducts(`el5`);
